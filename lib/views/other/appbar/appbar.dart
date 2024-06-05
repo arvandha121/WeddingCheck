6ee767cf83c57cf852b181708c6abb5c7143cfd6
arvandha121 <nauvan121@gmail.com>
2024-06-05 12:23:14 +0700

    tambah information untuk download import list tamu, dan tambah popup untuk keluar dari aplikasi (bukan logout)

--- lib/views/other/appbar/appbar.dart
+++ lib/views/other/appbar/appbar.dart
@@ -1,16 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:weddingcheck/app/provider/provider.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({Key? key}) : super(key: key);
+  MyAppBar({Key? key}) : super(key: key);
+
+  final Uri url = Uri.parse('https://excelexample.arvandhaa.my.id');
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text("Wedding Check"),
       backgroundColor: Colors.deepPurple,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.info),
+          tooltip: 'Information',
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  title: Text(
+                    "Template Import Excel",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Klik button di bawah untuk mengunduh template excel list tamu",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton.icon(
+                        icon: Icon(Icons.download, color: Colors.white),
+                        label: Text('Download Template'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                          onPrimary: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                        ),
+                        onPressed: () async {
+                          _launchUrl();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ],
     );
   }
 
--- lib/views/other/menu/screens/homes-parent/homes.dart
+++ lib/views/other/menu/screens/homes-parent/homes.dart
@@ -90,149 +90,176 @@ class _HomesParentState extends State<HomesParent> {
     );
   }
 
+  Future<bool> _onWillPop() async {
+    return await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Konfirmasi Keluar'),
+            content: Text('Apakah Anda yakin ingin keluar dari aplikasi?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('Tidak'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: Text(
+                  'Iya',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
     Color textColor = isDarkMode ? Colors.white : Colors.black;
-    return Scaffold(
-      body: FutureBuilder<List<ParentListItem>>(
-        future: DatabaseHelper().getParent(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
-          } else if (snapshot.hasData && snapshot.data!.isEmpty) {
-            return Center(
-              child: Text(
-                "Data Tidak Ditemukan",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey),
-              ),
-            );
-          } else if (snapshot.hasData) {
-            final parents = snapshot.data!;
-            return Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "List Berkas",
-                    style: TextStyle(
-                      fontSize: 22,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: FutureBuilder<List<ParentListItem>>(
+          future: DatabaseHelper().getParent(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Error: ${snapshot.error}"));
+            } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+              return Center(
+                child: Text(
+                  "Data Tidak Ditemukan",
+                  style: TextStyle(
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+              );
+            } else if (snapshot.hasData) {
+              final parents = snapshot.data!;
+              return Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "List Berkas",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: parents.length,
-                    itemBuilder: (context, index) {
-                      final item = parents[index];
-                      return Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        margin: EdgeInsets.all(8),
-                        elevation: 4,
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 10,
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: parents.length,
+                      itemBuilder: (context, index) {
+                        final item = parents[index];
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          title: Text(
-                            item.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  top: 5.0,
-                                  bottom: 2.0,
-                                ),
-                                child: Text(
-                                  '${item.namapria} & ${item.namawanita}',
-                                  style: TextStyle(
-                                    fontSize: 14,
+                          margin: EdgeInsets.all(8),
+                          elevation: 4,
+                          child: ListTile(
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            title: Text(
+                              item.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 5.0,
+                                    bottom: 2.0,
+                                  ),
+                                  child: Text(
+                                    '${item.namapria} & ${item.namawanita}',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(height: 4),
-                              Text(
-                                'Ketuk untuk melihat detail',
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  color: Colors.grey[600],
-                                ),
-                              )
-                            ],
-                          ),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.edit, color: Colors.orange),
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => EditParents(
-                                        item: item,
-                                        onEditSuccess: onEditSuccess,
+                                SizedBox(height: 4),
+                                Text(
+                                  'Ketuk untuk melihat detail',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    color: Colors.grey[600],
+                                  ),
+                                )
+                              ],
+                            ),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.edit, color: Colors.orange),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => EditParents(
+                                          item: item,
+                                          onEditSuccess: onEditSuccess,
+                                        ),
                                       ),
-                                    ),
-                                  );
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.delete, color: Colors.red),
-                                onPressed: () => _confirmDelete(
-                                  item.id!,
-                                  item.title,
-                                  textColor,
+                                    );
+                                  },
                                 ),
-                              ),
-                            ],
+                                IconButton(
+                                  icon: Icon(Icons.delete, color: Colors.red),
+                                  onPressed: () => _confirmDelete(
+                                    item.id!,
+                                    item.title,
+                                    textColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomesChild(
+                                    parentId: item.id!,
+                                  ),
+                                ),
+                              );
+                            },
                           ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomesChild(
-                                  parentId: item.id!,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CreateParent(),
+              ),
             );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CreateParent(),
-            ),
-          );
-        },
-        backgroundColor: Colors.deepPurple,
-        child: Icon(Icons.add, color: Colors.white),
+          },
+          backgroundColor: Colors.deepPurple,
+          child: Icon(Icons.add, color: Colors.white),
+        ),
       ),
     );
   }
